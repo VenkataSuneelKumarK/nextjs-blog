0ef2c634bb4277b5cd01ad897f86f6b4ddc5ab5d
Venkata Suneel Kumar K <suneelkumar.261989@gmail.com>
2023-11-12 13:31:27 +0530

    blog code

---
+++ pages/index.js
@@ -0,0 +1,73 @@
+import Head from "next/head";
+import Link from "next/link";
+import Layout, { siteTitle } from "../components/layout";
+import utilStyles from "../styles/utils.module.css";
+import { getSortedPostsData } from "../lib/posts";
+import Date from "../components/date";
+// import styles from "../styles/Home.module.css";
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+// export async function getServerSideProps(context) {
+//   return {
+//     props: {
+//       // props for your component
+//     },
+//   };
+// }
+
+export default function Home({ allPostsData }) {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>Hello, I'm Suneel. I'm a software engineer</p>
+        <p>
+          (This is a sample website - you’ll be building a site like this on{" "}
+          <Link href="/posts/first-post">our Next.js tutorial</Link>.)
+        </p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            // <li className={utilStyles.listItem} key={id}>
+            //   {title}
+            //   <br />
+            //   {id}
+            //   <br />
+            //   {date}
+            // </li>
+            <li className={utilStyles.listItem} key={id}>
+              <Link href={`/posts/${id}`}>{title}</Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </Layout>
+  );
+}
+
+
+// import useSWR from "swr";
+
+// function Profile() {
+//   const { data, error } = useSWR("/api/user", fetch);
+
+//   if (error) return <div>failed to load</div>;
+//   if (!data) return <div>loading...</div>;
+//   return <div>hello {data.name}!</div>;
+// }
